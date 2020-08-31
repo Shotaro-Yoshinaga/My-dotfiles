2d97039e28a1b1adc7f75d8f43244ed378af4810
Shotaro-Yoshinaga <shotaro19963094@gmail.com>
2020-08-31 22:14:36 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ DOT_FILES=(.zshrc .bash_profile ...)
 for file in ${DOT_FILES[@]}
 do
 	ln -s $HOME/dotfiles/$file $HOME/$file
-	ln -s .vimrc ~/.vimrc
-	ln -s .zshrc ~/.zshrc
-	ln -s .bash_brofile ~/.bash_profile
-	ln -s .bashrc ~/.bashrc
+	ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+	ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+	ln -s $HOME/dotfiles/.bash_brofile $HOME/.bash_profile
+	ln -s $HOME/dotdiles/.bashrc $HOME/.bashrc
 done
